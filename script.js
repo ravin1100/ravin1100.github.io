ad73fc5470eb2ba84ae5dff9ed9e81f22b53b91b
Ravin Kumar Jangir <ravinjangir.rj1100@gmail.com>
2023-10-03 08:33:19 +0530

    V1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var typed = new Typed(".text", {
   strings: ["Front End Developer", "Java Backend Developer"],
   typeSpeed: 50,
@@ -5,3 +6,12 @@ var typed = new Typed(".text", {
   baskDelay: 1000,
   loop: true,
 });
+=======
+var typed = new Typed(".text", {
+  strings: ["Front End Developer", "Java Backend Developer"],
+  typeSpeed: 50,
+  backSpeed: 100,
+  baskDelay: 1000,
+  loop: true,
+});
+>>>>>>> 72c2d96bd7c2beb91a39a7a272e9f790f138c4cf
