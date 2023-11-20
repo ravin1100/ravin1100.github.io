ca4fe4dc14d27d605f342f17c986b50c6e36da39
Ravin Kumar Jangir <ravinjangir.rj1100@gmail.com>
2023-11-20 21:03:11 +0530

    responsive design feature added

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ var typed = new Typed(".text", {
   baskDelay: 1000,
   loop: true,
 });
+
+// $(document).ready(function () {
+//   $(window).scroll(function () {
+//     // sticky navbar on scroll script
+//     if (this.scrollY > 20) {
+//       $(".navbar").addClass("sticky");
+//     } else {
+//       $(".navbar").removeClass("sticky");
+//     }
+//   });
+// });
